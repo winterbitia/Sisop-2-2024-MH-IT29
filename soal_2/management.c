0f098a27e702edcdd18ba4897c3a6ca6196fcf27
winterbitia <amoes.noland@gmail.com>
2024-04-23 23:09:25 +0700

    added log writing, signal still doesnt work

--- soal_2/management.c
+++ soal_2/management.c
@@ -12,6 +12,7 @@
 #include <syslog.h>
 #include <libgen.h>
 #include <dirent.h>
+#include <time.h>
 
 /* Soal_2 - VERSION 0.5
 Amoes Noland 5027231028
@@ -21,21 +22,65 @@ Amoes Noland 5027231028
 #define LIBRARY     "https://drive.google.com/uc?export=download&id=1rUIZmp10lXLtCIH3LAZJzRPeRks3Crup"
 #define MAX_BUFFER  1024
 char dir_name[MAX_BUFFER];
-static int got = 0;
+
+// Log setup
+void log_write(char *name, int act_code){
+    char  dir_log[MAX_BUFFER]; 
+    strcpy(dir_log, dir_name);
+    strcat(dir_log, "/history.log");
+
+    FILE *file;
+    file = fopen(dir_log, "a");
+    if (!file) return; 
+    
+    char uname[MAX_BUFFER]; getlogin_r(uname, sizeof(uname));
+    char buffer[MAX_BUFFER];
+    time_t timevar; struct tm *timeinfo;
+    time (&timevar); timeinfo = localtime (&timevar);
+
+    // 1 == rename, 2 == delete, 3 == backup, 4 == restore
+    switch (act_code)
+    {
+    case 1:
+        sprintf(buffer, "[%s][%02d:%02d:%02d] - %s - Succesfully renamed.",
+        uname, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, name);
+        break;
+    case 2:
+        sprintf(buffer, "[%s][%02d:%02d:%02d] - %s - Succesfully deleted.",
+        uname, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, name);
+        break;
+    case 3:
+        sprintf(buffer, "[%s][%02d:%02d:%02d] - %s - Succesfully moved to backup.",
+        uname, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, name);
+        break;
+    case 4:
+        sprintf(buffer, "[%s][%02d:%02d:%02d] - %s - Succesfully restored from backup.",
+        uname, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec, name);
+        break;
+    }
+
+    fprintf(file, "%s\n", buffer);
+    fclose(file);
+}
 
 // Set mode signals
 volatile sig_atomic_t mode = 0;
-void signal_default(int sig) {
-    mode = 0;
-}
-void signal_backup(int sig) {
-    mode = 1;
-}
-void signal_restore(int sig) {
-    mode = 2;
-}
-void signal_exit(int sig) {
-    exit(EXIT_SUCCESS);
+void signal_handler(int sig){
+    switch (sig)
+    {
+    case 34:
+        mode = 0;
+        break;
+    case SIGUSR1:
+        mode = 1;
+        break;
+    case SIGUSR2:
+        mode = 2;
+        break;
+    case SIGTERM:
+        exit(EXIT_SUCCESS);
+        break;
+    }
 }
 
 // Deletes all library starting files
@@ -84,6 +129,27 @@ void reset_default(){
         int status;
         waitpid(pid, &status, 0);
     }
+    }
+}
+
+void touch_log(){
+    pid_t pid = fork();
+    if (pid < 0) {
+        printf("Error: Fork failed\n");
+        exit(1);
+    }
+    if (0 == pid){
+        // Child : exec process
+        chdir(dir_name);
+        char *cmd = "/usr/bin/touch";
+        char *arg[] = {"touch", "history.log", NULL};
+        execvp(cmd,arg);
+        exit(0);
+    }
+    else {
+        // Parent : wait for child to die
+        int status;
+        waitpid(pid, &status, 0);
     }
 }
 
@@ -141,7 +207,7 @@ char rot19(char input){
 
 // Main rename loop
 void default_mode(){
-    char *dir_lib; 
+    char dir_lib[MAX_BUFFER]; 
     strcpy(dir_lib, dir_name);
     strcat(dir_lib, "/library");
     char buffer[MAX_BUFFER], translate[MAX_BUFFER];
@@ -164,18 +230,23 @@ void default_mode(){
         }
         // Main checks for actions
         if(strstr(buffer, "d3Let3") != NULL){
+            log_write(buffer, 2);
             remove(buffer);
         } else if(strstr(buffer, "r3N4mE") != NULL){
             if(strstr(buffer, ".ts") != NULL){
+                log_write(buffer, 1);
                 rename(buffer, "helper.ts");
             } 
             else if(strstr(buffer, ".py") != NULL){
+                log_write(buffer, 1);
                 rename(buffer, "calculator.py");
             } 
             else if(strstr(buffer, ".go") != NULL){
+                log_write(buffer, 1);
                 rename(buffer, "server.go");
             } 
             else{
+                log_write(buffer, 1);
                 rename(buffer, "renamed.file");
             } 
         } else if(strstr(buffer, "m0V3") != NULL){
@@ -184,7 +255,6 @@ void default_mode(){
         // sleep(1);
     }
     closedir(dir);
-    raise(SIGUSR1);
     return;
 }
 
@@ -279,6 +349,7 @@ void branch_mode(int backup){
 
         if (backup) backup_move(buffer_lib, buffer_bak);
         else        backup_move(buffer_bak, buffer_lib);
+        log_write(buffer, 4-backup);
         // sleep(1);
     }
     closedir(dir);
@@ -287,9 +358,11 @@ void branch_mode(int backup){
 
 void branch_default(){
     // Obtain library only once
+    static int got = 0;
     if (!got)
     {
         reset_default();
+        touch_log();
         get_library();
         ext_library();
         default_mode();
@@ -297,27 +370,26 @@ void branch_default(){
     }
 }
 
+void set_signals(){
+    // Attempt to grab signals
+    signal(SIGRTMIN, signal_handler);
+    signal(SIGUSR1,  signal_handler);
+    signal(SIGUSR2,  signal_handler);
+    signal(SIGTERM,  signal_handler);
+}
+
 int main(int argc, char *argv[]){
     // Save current directory to global var
     getcwd(dir_name, sizeof(dir_name));
 
-    // Attempt to grab signals
-    signal(SIGRTMIN, signal_default);
-    signal(SIGUSR1, signal_backup);
-    signal(SIGUSR2, signal_restore);
-    signal(SIGTERM, signal_exit);
-    
+    set_signals();
+
     // Select mode from args
-    if (argc == 1) mode = 0;
-    else if ((argc == 3)&&(strcmp(argv[1], "-m") == 0)){
+    if ((argc == 3)&&(strcmp(argv[1], "-m") == 0)){
         // Check for mode arguments
         if (strcmp(argv[2], "backup") == 0)  mode = 1;
         if (strcmp(argv[2], "restore") == 0) mode = 2;
-    } else return 0;
-
-    // printf("%s", dir_name);
-    // get_library();
-    // ext_library();
+    }
 
     pid_t pid = fork();
     if (pid < 0) exit(EXIT_FAILURE);
@@ -337,18 +409,17 @@ int main(int argc, char *argv[]){
     while (1)
     {
         switch(mode){
-            case 0:
+            case (0):
                 branch_default();
                 break;
-            case 1:
+            case (1):
                 branch_mode(1);
                 break;
-            case 2:
+            case (2):
                 branch_mode(0);
                 break;
         }
-        sleep(10);
+        sleep(1000);
     }
-
     return 0;    
 }
